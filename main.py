36bc6263634cbe191b7fa6437062e18c6f2fceea
Matthew Wong <mp2wong@edu.uwaterloo.ca>
2017-12-02 17:58:39 -0500

    Fixed matthew's shitty code

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import re
 
 from Word import Word
 from Sentence import Sentence
+from Question import Question
 
 if __name__ == "__main__":
     client = language.LanguageServiceClient()
-    text = u'The Golden Gate is in Bell High School. It is a cool Golden Gate Bridge.'
+    text = u'Socrates was a philosopher who taught many young minds. Socrates even taught Plato another great philosopher. This great philosopher lived in the fifth century B.C. Socrates was wrongly accused and sentenced to death, but his knowledge lives on.  Socrates was born around 470 B.C. Socrates was born into a poor family. Socrates was born in a village on the side of Mount Lycabettus. His father was a sculptor who was excellent at his job. When he was old enough his father taught him how to be sculptor but his creations always came out less than acceptable in the eyes of the clients. His mother was a mid wife. She helped deliver a lot of the children in Athens.'
 
     document = types.Document(
         content=text,
@@ -60,6 +61,8 @@ if __name__ == "__main__":
 
     sentenceList = Sentence.seperate_sentences(words)
     sentenceList = Sentence.update_subject(sentenceList)
-    for sentence in sentenceList:
-        sentence.print_sentence()
+
+    questions = [Question(sentenceList) for sentenceList in sentenceList if Question.is_question(sentenceList)]
+    for question in questions:
+        print(question.export())
 
