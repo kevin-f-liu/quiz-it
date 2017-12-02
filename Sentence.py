2a848dc90a98fe92cb866c18bda5555251dfc919
Emily Zeng <zhixuan.zeng@gmail.com>
2017-12-02 18:26:57 -0500

    sentences are working

--- Sentence.py
+++ Sentence.py
@@ -10,9 +10,56 @@ from copy import deepcopy
 class Sentence:
     __subject_carry_over = ['this', 'it', 'he', 'she', 'his', 'her']
 
-    def __init__(self, words):
-        self.words = words
+    def __init__(self, text):
         self.subject = None
+        client = language.LanguageServiceClient()
+
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT)
+
+        entities = client.analyze_entities(document).entities
+        tokens = client.analyze_syntax(document).tokens
+
+        self.words = []
+        entity_list = []
+
+        # words_content_list makes it easier to group words later on
+        # basically the string content of the words
+        words_content_list = []
+
+        # add word objects to Word list
+        # add string content into words_content_list
+        for token in tokens:
+            self.words.append(Word(token))
+            words_content_list.append(token.text.content)
+
+        # includes all mentions in the entity_list
+        for entity in entities:
+            for mention in entity.mentions:
+                copy = deepcopy(entity)
+                copy.name = mention.text.content
+                entity_list.append(copy)
+
+        entity_list.sort(key=lambda item: (-len(item.name), item.name))
+
+        # replace the words with entities if possible
+        # group together words appearing in the same entity
+        for entityContent in entity_list:
+            entity_content_arr = entityContent.name.split(' ')
+            for i in range(len(words_content_list)):
+                if i >= len(self.words):
+                    break
+                if words_content_list[i] == entity_content_arr[0] \
+                        and words_content_list[i:i + len(entity_content_arr)] == entity_content_arr:
+                    del self.words[i + 1:i + len(entity_content_arr)]
+                    self.words[i].add_entity(entityContent)
+                    words_content_list[i] = entityContent.name
+                    for j in range(i + 1, i + len(entity_content_arr)):
+                        words_content_list[j] = 0
+                        # words.insert(i, entityContent)
+                        #  replace this with actually creating a word object that has the appropriate attributes
+                        # provided by entity (this means modifying the word object to be able to init a entity word)
 
     def __str__(self):
         return self.return_string()
@@ -41,17 +88,10 @@ class Sentence:
         return sentence_list
 
     @staticmethod
-    def seperate_sentences(word_list):
-        sentence_list = []
-        sentence = []
-        for word in word_list:
-            if word.content == ".":
-                sentence_obj = Sentence(sentence)
-                sentence_list.append(sentence_obj)
-                sentence = []
-            else:
-                sentence.append(word)
-        return sentence_list
+    def seperate_sentences(text):
+        sentence_list = text.split('. ')
+
+        return [Sentence(sentence) for sentence in sentence_list]
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -19,58 +19,20 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\
 def GetTextFromImage(imagePath):
     return pytesseract.image_to_string(Image.open('imagePath'))
 
+
 if __name__ == "__main__":
     client = language.LanguageServiceClient()
-    text = u'Socrates was a philosopher who taught many young minds. Socrates even taught Plato another great philosopher. This great philosopher lived in the fifth century B.C. Socrates was wrongly accused and sentenced to death, but his knowledge lives on.  Socrates was born around 470 B.C. Socrates was born into a poor family. Socrates was born in a village on the side of Mount Lycabettus. His father was a sculptor who was excellent at his job. When he was old enough his father taught him how to be sculptor but his creations always came out less than acceptable in the eyes of the clients. His mother was a mid wife. She helped deliver a lot of the children in Athens.'
+    text = u'Socrates was a philosopher who taught many young minds. \
+            Socrates even taught Plato another great philosopher. \
+            This great philosopher lived in the fifth century B.C. \
+            Socrates was wrongly accused and sentenced to death, but his knowledge lives on. \
+            Socrates was born around 470 B.C. Socrates was born into a poor family. \
+            Socrates was born in a village on the side of Mount Lycabettus. \
+            His father was a sculptor who was excellent at his job. When he was old enough his father taught \
+            him how to be sculptor but his creations always came out less than acceptable in the eyes of the clients. \
+            His mother was a mid wife. She helped deliver a lot of the children in Athens.'
 
-    document = types.Document(
-        content=text,
-        type=enums.Document.Type.PLAIN_TEXT)
-
-    entities = client.analyze_entities(document).entities
-    tokens = client.analyze_syntax(document).tokens
-
-    words = []
-    entityList = []
-
-    # wordsContentList makes it easier to group words later on
-    # basically the string content of the words
-    wordsContentList = []
-
-    # add word objects to Word list
-    # add string content into wordsContentList
-    for token in tokens:
-        words.append(Word(token))
-        wordsContentList.append(token.text.content)
-
-    # includes all mentions in the entityList
-    for entity in entities:
-        for mention in entity.mentions:
-            entity.name = mention.text.content
-            entityList.append(entity)
-
-    #sorts the entity list by length shortest to longest
-    entityList.sort(key=lambda item: (-len(item.name), item.name))
-
-    # replace the words with entities if possible
-    # group together words appearing in the same entity
-    matchIndices = []
-    for entityContent in entityList:
-
-        entityContentArr = entityContent.name.split(' ')
-        # 
-        for i in range(len(wordsContentList)):
-            if i >= len(words):
-                break
-            if wordsContentList[i] == entityContentArr[0] \
-                    and wordsContentList[i:i + len(entityContentArr)] == entityContentArr:
-                del words[i + 1:i + len(entityContentArr)]
-                words[i].add_entity(entityContent)
-                wordsContentList[i] = entityContent.name
-                for j in range(i+1, i+len(entityContentArr)):
-                    wordsContentList[j] = 0
-
-    sentenceList = Sentence.seperate_sentences(words)
+    sentenceList = Sentence.seperate_sentences(text)
     sentenceList = Sentence.update_subject(sentenceList)
 
     questions = [Question(sentenceList) for sentenceList in sentenceList if Question.is_question(sentenceList)]
