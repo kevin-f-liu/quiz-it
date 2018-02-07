3fd1b6be6e1bc361837e77ee2206228bf95462a1
kevin-f-liu <kevin.f.liu2012@gmail.com>
2018-02-06 23:31:02 -0500

    Remove extra service keys

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,9 +1,15 @@
-from googleapiclient.discovery import build
+from google.cloud import language
+from google.cloud.language import enums
+from google.could.language import types
 import json
 
 
 if __name__ == "__main__":
     # Init natural language processing service with API key
     # API keys do not identify the user/application
-    service = build('language', 'v1', developerKey='AIzaSyBcSxRU7-u2ietvX0XlkFW8KMblQMQx3w4')
-    request = language.documents.annotateText(document="asdfasdf")
+    # service = build('language', 'v1', developerKey='AIzaSyBcSxRU7-u2ietvX0XlkFW8KMblQMQx3w4')
+    # request = language.documents.annotateText(document="asdfasdf")
+
+    # Using Google Cloud Client Library
+    client = language.LanguageServiceClient()
+    
